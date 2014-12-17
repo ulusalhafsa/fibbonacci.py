0e1e588e1e45126c96eaf81b5a3f2ffe12ae2c8c
ulusalhafsa <hafsaulusal@bil.omu.edu.tr>
2014-12-17 22:01:43 +0200

    Update fibbonacci.py

--- fibbonacci.py
+++ fibbonacci.py
@@ -1,40 +1,64 @@
-def dec2fib(dec):
-	sayi = int(dec)
-	i=1
-	j=0
-	dizi1,dizi2 =[],[]
-	while dec>=1:
-		while fib[i][-1] < dec:
-			i=i+1
-		i=i-1     
-		if dec==sayi:     
-			p=i+1 
-		dizi2.insert(0,fib[i][-1])     
-		dec=dec-fib[i][-1]
-		i = 1
-	while j<p+1:     
-		if fib[p][j] in dizi2:
-			dizi1.insert(0,"1") 
-		else:
-			dizi1.insert(0,"0")
-		j=j+1
-		sonuc = ''.join(dizi1)     
-	return str(sonuc)
-	
 
+
+#!/usr/bin/env python
+# - coding=UTF-8  -
+def fibonacci(n):     
+        i = 0
+	dizi1= []
+	while i <= n:     
+		if i < 2:
+			dizi1.append(1)
+		else:
+			dizi1.append(dizi1[i-1] + dizi1[i-2])
+		i=i+1
+	return dizi1
+ 
+def dec2fib(decimal):
+	x= int(decimal)
+	i = 1
+        j = 0
+        dizi2= []
+        dizi3= []
+ 
+	while decimal > 0:
+		while fibonacci(i)[-1] < decimal:
+			i=i+1
+		i=i-1   
+		if decimal == x:     
+			k = i +1 
+		dizi3.append(fibonacci(i)[-1])     
+		decimal=decimal-fibonacci(i)[-1]
+		i = 1
+	
+        while k+1>j:     
+		if fibonacci(k)[j] in dizi3:
+			dizi2.append("1")
+			dizi3[-1] = 0.5     
+		else:
+			dizi2.append("0")
+		j=j+1
+		sonuc = ''.join(dizi2)     
+	
+	return sonuc
+ 
 def fib2dec(fib):
 	dizi1= str(fib)
-	j=0
-	dec=0
-	while j<len(dizi1):
-		if dizi1[j]=="1":    
-			dec=dec+fib[len(dizi1)][j]
-		j=j+1
-	return dec
+	i=0
+        decimal=0
  
-if dec2fib(fib2dec(('100101000110010'))==100101000110010:
+	while len(dizi1)>i:
+		if dizi1[i] == "1":     
+            		decimal=decimal+fibonacci(len(dizi1))[i]
+		i=i+1
+	return decimal
+ 
+if dec2fib(fib2dec('1010001010'))=='1010001010':
   print("test1 OK")
  
-if dec2fib(100) == '101001000010':
+if dec2fib(100) == '101001000010': 
   print("test2 OK")
+ 
+	
+
+
  	
